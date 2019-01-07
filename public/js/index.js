8abf60f8d51a92bb6cb6ad9694bc16fe13181b1b
Paul <pbriar@live.com>
2019-01-06 21:12:05 -0400

    Added geolocation support via api & location tests

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,15 @@
 let socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', () => {
     console.log('Connected to server');
-
 });
 
-socket.on('disconnect', function() {
+socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
 
-socket.on('newMessage', function(message) {
+socket.on('newMessage', (message) => {
     console.log('newMessage', message);
 
     let newLi = document.createElement('LI');
@@ -18,16 +17,36 @@ socket.on('newMessage', function(message) {
     newLi.appendChild(newContent);
     document.querySelector('#messages').append(newLi);
 });
+//Send geolocation to other user
+socket.on('newLocationMessage', (message) => {
+    let newLi = jQuery('<li></li>');
+    let a = jQuery('<a target="_blank">My Location</a>');
 
-document.querySelector('#message-form').addEventListener('submit', function(event) {
+    newLi.text(`${message.from}: `);
+    a.attr('href', message.url);
+    newLi.append(a);
+    jQuery('#messages').append(newLi);
+});
+
+document.querySelector('#message-form').addEventListener('submit', (event) => {
     event.preventDefault();
 
     socket.emit('createMessage', {
         from: 'User',
         text: document.querySelector('[name=message]').value,
-    }, function() {
+    }, () => {});
+});
 
-    });
+let locationButton = document.querySelector('#send-location').addEventListener('click', (event) => {
+    if (!navigator.geolocation) {
+        return alert('Sorry, your browser does not support geolocation');
+    }
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        });
+    }, err => alert('Unable to fetch location'));
 });
 
 
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,12 +1,21 @@
 let expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generate message', () => {
     it('Should generate the correct message object', () => {
         let res = generateMessage('Paul', 'Hello');
             expect(res.from).toBe('Paul')
             expect(res.text).toBe('Hello')
-            expect(typeof res.createdAt).toBe('number')
+            expect(res.createdAt).toBeA('number')
+    });
+});
+
+describe('generate location url', () => {
+    it('Should generate location', () => {
+        let res = generateLocationMessage('admin', 15, -19);
+        expect(res.from).toBe('admin')
+        expect(res.url).toBe('https://www.google.com/maps?q15,-19')
+        expect(res.createdAt).toBeA('number')
     });
 });
